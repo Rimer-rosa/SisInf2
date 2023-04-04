dc2c511289de7aa3a3c2ce3b0b3b882e5a31d3cf
kevinrioja <kevinrioja216@gmail.com>
2023-04-03 22:31:54 -0400

    Modificacion: aniadi cosas a historia de usuarioDao

--- proyectoUS/src/main/java/datos/Historia_de_usuarioDAO.java
+++ proyectoUS/src/main/java/datos/Historia_de_usuarioDAO.java
@@ -4,6 +4,10 @@
  */
 package datos;
 
+import dominio.Historia_de_usuario;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Windows
@@ -14,4 +18,56 @@ public class Historia_de_usuarioDAO {
     public String SQL_update = "update equipos set nombre= ? ,puntos=? where idEquipos=?;";
     public String SQL_delete = "delete from equipos where idEquipos=?;";
     public String SQL_search = "select * from equipos where nombre= ? and puntos=?;";
+    
+    
+    
+     public int insertar(Historia_de_usuario historia){
+        Connection conexion = null;
+        PreparedStatement sentencia = null;
+        int registros = 0;
+        
+        try {
+            conexion = ConexionBD.getConexion();
+            sentencia = conexion.prepareStatement(SQL_insert);
+            sentencia.setInt(1,historia.getEstado_idestado());
+            sentencia.setString(2, historia.getNombre());
+            sentencia.setString(3, historia.getDescripcion());
+            sentencia.setString(4,historia.getCondiciones());
+            System.out.println("importancia: "+historia.getImportancia()+"dificultad: "+historia.getDificultad());
+            sentencia.setInt(5, historia.getImportancia());
+            sentencia.setInt(6, historia.getDificultad());
+            registros = sentencia.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Equipo Registrado");
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }finally{
+            try{
+                ConexionBD.cerrar(sentencia);
+                ConexionBD.cerrar(conexion);
+            }catch(SQLException e){
+                e.printStackTrace(System.out);
+            }
+        }
+        System.out.println("registros: "+registros);
+        return registros;
+    }
+    
+    
+    
+    
+    
+    
+    
+    public int empty(String nombre){
+        int resultado;
+        boolean vacio;
+        
+        vacio = nombre.equals("");
+        if(vacio){
+            resultado = 0;
+        }else{
+            resultado = 1;
+        }
+        return resultado;
+    }
 }
